aaeb95259c9a8be4314fc8169d9741043e7bbdd9
Olamide Olayemi <olamid15@gmail.com>
2025-06-20 19:08:28 +0100

    Add dotenv for environment variable management and update dependencies

--- server/src/stream-client.ts
+++ server/src/stream-client.ts
@@ -1,6 +1,10 @@
 import { StreamClient } from "@stream-io/node-sdk";
 
-const apiKey = "8uchjuawduc4";
-const apiSecret = "ukv9tqxj7x4bjcturs4dstza4hgx8vz33kubk92d53ghs2z4kegscn4v3kcpknf4";
+const apiKey = process.env.STREAM_API_KEY;
+const apiSecret = process.env.STREAM_API_SECRET;
 
-export const client = new StreamClient(apiKey, apiSecret)
+if (!apiKey || !apiSecret) {
+  throw new Error("STREAM_API_KEY and STREAM_API_SECRET must be set in environment variables");
+}
+
+export const client = new StreamClient(apiKey as string, apiSecret as string)
